37736f2573a704a860a57602518f6ca133c8e004
Syed Basim Ali <sy8bsxm@gmail.com>
2026-09-14 20:15:28 +0530

    Time: 2 ms (77.35%) | Memory: 7.8 MB (37.95%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int fib(int n) {
+        if (n == 0) {
+            return 0;
+        }
+
+        int a = 0;
+        int b = 1;
+        int count = 2;
+
+        while (count <= n) {
+            int temp = b;
+            b = b + a;
+            a = temp;
+            count++;
+        }
+
+        return b;
+    }
+};
